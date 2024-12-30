d4e9fd753995dfa9cd09c9710f1594dcfab03e8e
astrick <astrick.harren.at.work@gmail.com>
2024-12-30 00:11:58 -0600

    feat: raw ctrl center

--- config.py
+++ config.py
@@ -1,47 +1,11 @@
-# pyright: reportIndexIssue=false, reportAttributeAccessIssue=false
-
-import datetime
-from ignis.widgets import Widget
-from ignis.utils import Utils
 from ignis.app import IgnisApp
-from ignis.services.audio import AudioService
-from ignis.services.system_tray import SystemTrayService, SystemTrayItem
-from ignis.services.hyprland import HyprlandService
-from ignis.services.niri import NiriService
-from ignis.services.notifications import NotificationService
-from ignis.services.mpris import MprisService, MprisPlayer
-
-from modules.clock import clock
-from modules.workspaces import workspaces
+from ignis.utils import Utils
+from modules.bar import bar
+from modules.control_center import control_center
 
 app = IgnisApp.get_default()
-
 app.apply_css(f"{Utils.get_current_dir()}/style.scss")
 
-
-audio = AudioService.get_default()
-system_tray = SystemTrayService.get_default()
-hyprland = HyprlandService.get_default()
-niri = NiriService.get_default()
-notifications = NotificationService.get_default()
-mpris = MprisService.get_default()
-
-
-def bar(monitor_id: int = 0) -> Widget.Window:
-    monitor_name = Utils.get_monitor(monitor_id).get_connector()  # type: ignore
-    return Widget.Window(
-        namespace=f"ignis_bar_{monitor_id}",
-        monitor=monitor_id,
-        anchor=["left", "top", "right"],
-        exclusivity="exclusive",
-        child=Widget.CenterBox(
-            css_classes=["bar"],
-            center_widget=Widget.Box(child = [workspaces()]),
-            end_widget = Widget.Box(child = [clock(monitor_id)]),
-        ),
-    )
-
-
-# this will display bar on all monitors
-for i in range(Utils.get_n_monitors()):
-    bar(i)
+control_center()
+for monitor in range(Utils.get_n_monitors()):
+    bar(monitor)
---
+++ modules/control_center/__init__.py
@@ -0,0 +1,51 @@
+# pyright: reportIndexIssue=false, reportAttributeAccessIssue=false
+
+from ignis.app import IgnisApp
+from ignis.widgets import Widget
+
+app = IgnisApp.get_default()
+
+
+def control_center_widget() -> Widget.Box:
+    return Widget.Box(
+        vertical=True,
+        css_classes=["control-center"],
+        child=[
+            Widget.Box(
+                vertical=True,
+                css_classes=["control-center-widget"],
+                child=[Widget.Label(label="hello, world")],
+            ),
+        ],
+    )
+
+
+def control_center() -> Widget.RevealerWindow:
+    revealer = Widget.Revealer(
+        transition_type="slide_left",
+        child=control_center_widget(),
+        transition_duration=300,
+        reveal_child=True,
+    )
+    box = Widget.Box(
+        child=[
+            Widget.Button(
+                vexpand=True,
+                hexpand=True,
+                css_classes=["unset"],
+                on_click=lambda x: app.close_window("ignis_CONTROL_CENTER"),
+            ),
+            revealer,
+        ],
+    )
+    return Widget.RevealerWindow(
+        visible=False,
+        popup=True,
+        kb_mode="on_demand",
+        layer="top",
+        css_classes=["unset"],
+        anchor=["top", "right", "bottom", "left"],
+        namespace="ignis_CONTROL_CENTER",
+        child=box,
+        revealer=revealer,
+    )
