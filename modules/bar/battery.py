15af2c0098a12f68e40bcb26eb4d4727f1894ee3
astrick <astrick.harren.at.work@gmail.com>
2025-01-13 22:33:21 -0600

    feat: battery charging indicator

--- modules/bar/battery.py
+++ modules/bar/battery.py
@@ -1,23 +1,58 @@
+import os
+
+from gi.repository import GdkPixbuf
+
+import ignis
 from ignis.services.upower import UPowerDevice, UPowerService
 from ignis.widgets import Widget
 
 upower = UPowerService.get_default()
 
 
+def flash_icon():
+    svg = os.path.join(os.path.dirname(__file__), "../../svg/flash.svg")
+    svg = open(svg, "r").read()
+    loader = GdkPixbuf.PixbufLoader()
+    loader.write(svg.encode())
+    loader.close()
+    return loader.get_pixbuf()
+
+
+class Variable(ignis.variable.Variable):
+    def __init__(self, refresh) -> None:
+        self.refresh = refresh
+        super().__init__(refresh())
+
+    def subscribe(self, obj, signal):
+        def call_back(*args):
+            self.value = self.refresh()
+
+        obj.connect(signal, call_back)
+
+    def set_value(self, val):
+        self.value = val
+
+
 class BatteryItem(Widget.Box):
     def __init__(self, device: UPowerDevice):
-        def css_classes(percent):
+        def css_classes():
             base = ["battery-slider"]
             normal = "battery-slider-normal"
             critical = "battery-slider-critical"
-            healthy = "battery-slider-healthy"
-
-            if percent <= 20:
+            charging = "battery-slider-charging"
+            if device.charging or device.charged:
+                return base + [charging]
+            if device.percent <= 20:
                 return base + [critical]
             return base + [normal]
 
+        css_classes = Variable(css_classes)
+        css_classes.subscribe(device, "notify::percent")
+        css_classes.subscribe(device, "notify::charging")
+        css_classes.subscribe(device, "notify::charged")
+
         self.scale = Widget.Scale(
-            css_classes=device.bind("percent", css_classes),
+            css_classes=css_classes.bind("value"),
             value=device.bind("percent"),
             step=1,
             max=100,
@@ -29,12 +64,25 @@ class BatteryItem(Widget.Box):
             css_classes=["txt", "mx-2"],
         )
 
+        connected_to_power = Variable(lambda: device.charging or device.charged)
+        connected_to_power.subscribe(device, "notify::charging")
+        connected_to_power.subscribe(device, "notify::charged")
+
+        self.charging_icon = Widget.Revealer(
+            child=Widget.Icon(image=flash_icon(), css_classes=["icon-sm"]),
+            reveal_child=connected_to_power.bind("value"),
+            transition_type="crossfade",
+        )
+
         super().__init__(
             css_classes=["mx-9"],
             setup=lambda self: device.connect("removed", lambda x: self.unparent()),
             child=[
                 self.label,
-                self.scale,
+                Widget.Overlay(
+                    child=self.scale,
+                    overlays=[self.charging_icon],
+                ),
                 Widget.Box(
                     valign="center",
                     css_classes=["battery-tip"],
